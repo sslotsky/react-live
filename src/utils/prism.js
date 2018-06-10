330b742c5b03902777440962b64bd3ff0021d55a
Sam Slotsky <sam@manifold.co>
2018-06-10 15:30:17 -0500

    Attempt to use babel plugin for prism dependencies

--- src/utils/prism.js
+++ src/utils/prism.js
@@ -1,9 +1,5 @@
-import { highlight, languages } from 'prismjs/components/prism-core'
-import 'prismjs/components/prism-clike'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-markup'
-import 'prismjs/components/prism-jsx'
+import Prism from 'prismjs';
 
-const prism = (code, language = 'jsx') => highlight(code, languages[language])
+const prism = (code, language = 'jsx') => Prism.highlight(code, Prism.languages[language])
 
 export default prism
